2064a58767038f2256991d331523527d2b86f5ed
koparasy <parasyris1@llnl.gov>
2024-08-28 18:16:18 -0700

    Modify example to include a global

--- examples/example.cu
+++ examples/example.cu
@@ -5,47 +5,47 @@
 #include <iostream>
 
 struct A{
-  int k;
-  double b, c[7];
+  double x,y;
 };
 
+__device__ A DINOS_DINOS = { 1.0, 1.0 };
+
 template <typename T> __global__ 
-void vecAdd(T *in, T *out, size_t size) {
+void vecAdd(T *in, T *out, T val, size_t size) {
   auto tid = threadIdx.x + blockIdx.x * blockDim.x;
   if (tid >= size)
     return;
   auto stride = gridDim.x * blockDim.x;
 
   for (; tid < size; tid += stride) {
-    out[tid] += in[tid];
+    out[tid] += in[tid] + val + DINOS_DINOS.x;
   }
 }
 
 int main(int argc, const char *argv[]) {
   size_t numElements = atoi(argv[1]);
-  float *in, *out;
-  cudaMalloc((void **)&in, numElements * sizeof(float));
-  cudaMalloc((void **)&out, numElements * sizeof(float));
+  double *in, *out;
+  double val = numElements;
+  cudaMalloc((void **)&in, numElements * sizeof(double));
+  cudaMalloc((void **)&out, numElements * sizeof(double));
   std::cout << "In : " << in << " Out " << out << "\n";
-  A tmp;
-  tmp.k = 42;
-  tmp.b = 123;
 
-  cudaMemset(in, 1, numElements * sizeof(float));
-  cudaMemset(out, 0, numElements * sizeof(float));
+  cudaMemset(in, 0, numElements * sizeof(double));
+  cudaMemset(out, 0, numElements * sizeof(double));
 
   const int threads = 256;
   int num_blocks = (numElements + threads - 1) / threads;
-  vecAdd<<< num_blocks, threads>>>(in, out, numElements);
+  vecAdd<<< num_blocks, threads>>>(in, out, 1024.0, numElements);
   cudaDeviceSynchronize();
 
-  float *h_in = new float[numElements];
-  float *h_out = new float[numElements];
-  cudaMemcpy(h_in, in, sizeof(float)*numElements, cudaMemcpyDeviceToHost);
-  cudaMemcpy(h_out, out, sizeof(float)*numElements, cudaMemcpyDeviceToHost);
+  double *h_in = new double[numElements];
+  double *h_out = new double[numElements];
+  cudaMemcpy(h_in, in, sizeof(double)*numElements, cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_out, out, sizeof(double)*numElements, cudaMemcpyDeviceToHost);
   for (int i = 0; i < numElements; i++){
-    if (h_in[i] != h_out[i]){
+    if (h_in[i] + 1.0 + val != h_out[i]){
       std::cout << "Values at " << i << " differ\n";
+      std::cout << "Values" << h_in[i] << " " << h_out[i] << "differ\n";
       return -1;
     }
   }
